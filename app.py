74abdf3c2821d155bf99519e36ca33e69457bac1
sahibpreet singh <ss9334931@gmail.com>
2025-02-01 17:32:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 def main():
     st.title("Hello World!")
     st.write("Welcome to my first Streamlits app")
+    st.write('made changes')
 
 if __name__ == "__main__":
     main()
